466c6a57cb04ae726dc9d521f6b9350a6fc27c85
valentine-ochieng <valentine.ochieng@student.moringaschool.com>
2021-09-13 12:42:48 +0300

    Implemented the hover effect using jquery

--- main.js
+++ main.js
@@ -43,3 +43,68 @@ $(document).ready(function () {
         })
     })
 })
+
+$(document).ready(function () {
+    $(".work3").mouseenter(function () {
+        $("#hover3").show()
+        $(".work3").mouseleave(function () {
+            $("#hover3").hide()
+        })
+    })
+})
+
+
+$(document).ready(function () {
+    $(".work2").mouseenter(function () {
+        $("#hover2").show()
+        $(".work2").mouseleave(function () {
+            $("#hover2").hide()
+        })
+    })
+})
+
+
+$(document).ready(function () {
+    $(".work1").mouseenter(function () {
+        $("#hover1").show()
+        $(".work1").mouseleave(function () {
+            $("#hover1").hide()
+        })
+    })
+})
+
+$(document).ready(function () {
+    $(".work5").mouseenter(function () {
+        $("#hover5").show()
+        $(".work5").mouseleave(function () {
+            $("#hover5").hide()
+        })
+    })
+})
+
+$(document).ready(function () {
+    $(".work6").mouseenter(function () {
+        $("#hover6").show()
+        $(".work6").mouseleave(function () {
+            $("#hover6").hide()
+        })
+    })
+})
+
+$(document).ready(function () {
+    $(".work7").mouseenter(function () {
+        $("#hover7").show()
+        $(".work7").mouseleave(function () {
+            $("#hover7").hide()
+        })
+    })
+})
+
+$(document).ready(function () {
+    $(".work8").mouseenter(function () {
+        $("#hover8").show()
+        $(".work8").mouseleave(function () {
+            $("#hover8").hide()
+        })
+    })
+})
